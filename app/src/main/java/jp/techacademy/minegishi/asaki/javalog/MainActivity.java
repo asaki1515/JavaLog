f57ddc90b2ffc906c7ce72428792be87117a5010
asaki <asaki.minegishi@music.yamaha.com>
2017-05-11 16:56:06 +0900

    カリキュラム5.4

--- app/src/main/java/jp/techacademy/minegishi/asaki/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/minegishi/asaki/javalog/MainActivity.java
@@ -11,15 +11,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);  // オーバーライドした関数内でAppCompatActivityクラスのonCreateメソッドを呼び出す。引数は上記onCreateメソッドで渡ってきた引数をそのまま渡す
         setContentView(R.layout.activity_main);  // MainActivityクラスがAppCompatActivityクラスからそのまま継承したメソッド
 
-        /*5.3*/
-        // 整数型の変数をnumという名前で作成して、10を代入する
-        int num = 10;
-        Log.d("javatest", String.valueOf(num));  // String.valueOf(num):数値型などの変数を文字列型として返すStringクラスの関数
-                                                 // Log.dメソッドの2つ目の引数には文字列型の値を渡す必要があるためString.valueOfで変換
-        // numに50を代入する
-        num = 50;
-        Log.d("javatest", String.valueOf(num));
+        /*5.4*/
+        int num1 = 10 + 5 - 2 * 4 / 2;
+        Log.d("javatest", "10 + 5 - 2 * 4 / 2 = " + String.valueOf(num1));  //文字列同士は+演算子によって連結
 
+        boolean flag1 = true;
+        boolean flag2 = false;
+        Log.d("javatest", "flag1 && flag2 = " + String.valueOf(flag1 && flag2));
+        Log.d("javatest", "flag1 || flag2 = " + String.valueOf(flag1 || flag2));
 
+        int num2 = 10;
+        int num3 = 20;
+        Log.d("javatest", "num2 < num3 = " + String.valueOf(num2 < num3));
+        
     }
 }
