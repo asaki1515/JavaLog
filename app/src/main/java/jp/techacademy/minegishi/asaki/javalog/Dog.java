4b02fdb0fd94512c7350d1440b8510eaca93f820
asaki <asaki.minegishi@music.yamaha.com>
2017-05-11 17:19:22 +0900

    カリキュラム9

--- app/src/main/java/jp/techacademy/minegishi/asaki/javalog/Dog.java
+++ app/src/main/java/jp/techacademy/minegishi/asaki/javalog/Dog.java
@@ -2,15 +2,10 @@ package jp.techacademy.minegishi.asaki.javalog;
 
 import android.util.Log;
 
-class Dog extends Animal{  //アクセスコントロール省略：同じパッケージ内のクラスからのみアクセス可能
-
+/*9*/
+class Dog extends Animal implements Movable {
     // クラス変数
     static String to_jp = "犬";
-
-    // メンバ変数
-    //String name;  // 名前
-    //int age;    // 年齢
-
 
     // コンストラクタ
     public Dog(String name, int age) {
@@ -24,7 +19,12 @@ class Dog extends Animal{  //アクセスコントロール省略：同じパッ
     }
 
     // メンバ関数
-    public void say() {  // メンバ関数だと、
+    public void say() {
         Log.d("javatest", this.name + "(" + this.age + "歳)" + "「ワンワン」");
+    }
+
+    @Override
+    public void move() {
+        Log.d("javatest", this.name + "(" + this.age + "歳)" + "は全力で走った。");
     }
 }
---
+++ app/src/main/java/jp/techacademy/minegishi/asaki/javalog/Movable.java
@@ -0,0 +1,6 @@
+package jp.techacademy.minegishi.asaki.javalog;
+
+interface Movable {
+    public void move();
+    //このインタフェースを実装するクラスは、必ず宣言されたpublic void moveメソッドを実装しなければいけない
+}
