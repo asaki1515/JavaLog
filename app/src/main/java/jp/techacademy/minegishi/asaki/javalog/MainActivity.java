b8adaf77b2341b7ac0f5ede79e0a63c31ccfa929
asaki <asaki.minegishi@music.yamaha.com>
2017-05-11 17:00:54 +0900

    カリキュラム5.6

--- app/src/main/java/jp/techacademy/minegishi/asaki/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/minegishi/asaki/javalog/MainActivity.java
@@ -11,34 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);  // オーバーライドした関数内でAppCompatActivityクラスのonCreateメソッドを呼び出す。引数は上記onCreateメソッドで渡ってきた引数をそのまま渡す
         setContentView(R.layout.activity_main);  // MainActivityクラスがAppCompatActivityクラスからそのまま継承したメソッド
 
-        /*5.5*/
-        int num = 60;
-
-        if (num >= 90) {
-            Log.d("javatest", "優");
-        } else if (num >= 75) {
-            Log.d("javatest", "良");
-        } else if (num >= 60) {
-            Log.d("javatest", "可");
-        } else {
-            Log.d("javatest", "不可");
+        /*5.6*/
+        for (int i = 1; i < 6; i++) {
+            Log.d("javatest", "for文の " + String.valueOf(i) + "回目");
         }
 
-        int drink = 1;
+        int num = 1;
 
-        switch (drink) {
-            case 0:
-                Log.d("javatest", "コーヒーを注文しました");
-                break;
-            case 1:
-                Log.d("javatest", "紅茶を注文しました");
-                break;
-            case 2:
-                Log.d("javatest", "ミルクを注文しました");
-                break;
-            default:
-                Log.d("javatest", "オーダーミスです");
-                break;
+        while (num < 6) {
+            Log.d("javatest", "while文の " + String.valueOf(num) + "回目");
+            num++;
         }
 
     }
